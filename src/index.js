ae8c5d45b07dade70df80742d8493047280300db
GauravMarathe <gauravmarathe6468@gmail.com>
2024-01-10 18:51:32 +0530

    profile

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ ReactDOM.render(
   <>
     <div className="cards">
       <div className="card ">
-        < img src="" alt="myPic" className="card_img" />
+        < img src="https://wallpapercave.com/w/wp4056410.jpg" alt="myPic" className="card_img" />
         <div className="card_info">
-          <span className="card_category"> </span>
-          <h3 className="card_title"> </h3>
+          <span className="card_category"> A Netflix Original Series </span>
+          <h3 className="card_title">Dark </h3>
           <a href="" target="_blank">
             <button> Watch Now </button>
           </a>
